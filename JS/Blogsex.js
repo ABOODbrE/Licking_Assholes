3381e22f2b8fd3f67c76789c635d53e78177a835
ABOODbrE <107629349+ABOODbrE@users.noreply.github.com>
2026-08-27 19:44:48 +0300

    Add files via upload
    Signed-off-by: ABOODbrE <107629349+ABOODbrE@users.noreply.github.com>

---
+++ JS/Blogsex.js
@@ -0,0 +1,95 @@
+<script>
+const slider = document.querySelector(".Dthumbinal-slide");
+const nextBtn = document.querySelector(".slider-next");
+const prevBtn = document.querySelector(".slider-prev");
+
+
+let autoSlide;
+
+
+function moveNext(){
+
+    slider.scrollLeft += slider.clientWidth / 4;
+
+
+    if(
+      slider.scrollLeft + slider.clientWidth 
+      >= slider.scrollWidth
+    ){
+
+        slider.scrollLeft = 0;
+
+    }
+
+}
+
+
+
+function movePrev(){
+
+    slider.scrollLeft -= slider.clientWidth / 4;
+
+
+    if(slider.scrollLeft <=0){
+
+        slider.scrollLeft =
+        slider.scrollWidth;
+
+    }
+
+}
+
+
+
+nextBtn.onclick=function(){
+
+    moveNext();
+
+}
+
+
+
+prevBtn.onclick=function(){
+
+    movePrev();
+
+}
+
+
+
+/* AUTO PLAY */
+
+function startAuto(){
+
+autoSlide=setInterval(
+moveNext,
+3000
+);
+
+}
+
+
+
+function stopAuto(){
+
+clearInterval(autoSlide);
+
+}
+
+
+
+slider.addEventListener(
+"mouseenter",
+stopAuto
+);
+
+
+slider.addEventListener(
+"mouseleave",
+startAuto
+);
+
+
+
+startAuto();
+</script>
